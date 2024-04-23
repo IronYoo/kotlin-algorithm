fedfc25f33fa093ea3822fbf41a73e5d178a0ae2
sceonn <yoou201@naver.com>
2024-04-23 18:59:12 +0900

    leetcode: 24

---
+++ src/main/kotlin/leetcode/swapNodesinPairs/Solution.kt
@@ -0,0 +1,20 @@
+package leetcode.swapNodesinPairs
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun swapPairs(head: ListNode?): ListNode? {
+        if (head?.next != null) {
+            val p = head.next
+            head.next = swapPairs(head.next!!.next)
+            if (p != null) {
+                p.next = head
+            }
+            return p
+        }
+
+        return head
+    }
+}
