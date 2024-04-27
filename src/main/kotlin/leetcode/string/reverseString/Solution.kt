46818ce8ceb0da6d49c042386e6e0b8e3fc2219b
sceonn <yoou201@naver.com>
2024-04-27 19:17:44 +0900

    leetcode: 344

---
+++ src/main/kotlin/leetcode/string/reverseString/Solution.kt
@@ -0,0 +1,14 @@
+package leetcode.string.reverseString
+
+class Solution {
+    fun reverseString(s: CharArray) {
+        var start = 0
+        var end = s.size - 1
+        while (start < end) {
+            s[start] = s[end].also { s[end] = s[start] }
+
+            start++
+            end--
+        }
+    }
+}
