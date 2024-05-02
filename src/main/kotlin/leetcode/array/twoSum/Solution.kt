12a97b81824ae73807389b5d195b7dd4f43fb087
sceonn <yoou201@naver.com>
2024-05-02 18:39:11 +0900

    leetcode:1

---
+++ src/main/kotlin/leetcode/array/twoSum/Solution.kt
@@ -0,0 +1,19 @@
+package leetcode.array.twoSum
+
+class Solution {
+    fun twoSum(
+        nums: IntArray,
+        target: Int,
+    ): IntArray {
+        val numsMap: MutableMap<Int, Int> = mutableMapOf()
+
+        for ((i, num) in nums.withIndex()) {
+            if (numsMap.containsKey(target - num)) {
+                return intArrayOf(numsMap[target - num]!!, i)
+            }
+            numsMap[num] = i
+        }
+
+        return intArrayOf(0, 0)
+    }
+}
