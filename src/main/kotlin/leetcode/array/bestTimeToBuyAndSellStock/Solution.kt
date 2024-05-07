48fbc033dce6441a36f08dd9840fe5c54d7b4964
sceonn <yoou201@naver.com>
2024-05-07 20:38:56 +0900

    leetcode: 121

---
+++ src/main/kotlin/leetcode/array/bestTimeToBuyAndSellStock/Solution.kt
@@ -0,0 +1,15 @@
+package leetcode.array.bestTimeToBuyAndSellStock
+
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        var maxProfit = 0
+        var minPrice = prices[0]
+
+        for (price in prices) {
+            minPrice = minPrice.coerceAtMost(price)
+            maxProfit = maxProfit.coerceAtLeast(price - minPrice)
+        }
+
+        return maxProfit
+    }
+}
