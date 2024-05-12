7251f3ffc407b031f0a3058ce7f33244c142ab22
sceonn <yoou201@naver.com>
2024-05-12 19:39:49 +0900

    leetcode: 232

---
+++ src/main/kotlin/leetcode/stack/implementQueueUsingStacks/Solution.kt
@@ -0,0 +1,29 @@
+package leetcode.stack.implementQueueUsingStacks
+
+import java.util.ArrayDeque
+import java.util.Deque
+
+class MyQueue {
+    val input: Deque<Int> = ArrayDeque()
+    val output = ArrayDeque<Int>()
+
+    fun push(x: Int) {
+        input.push(x)
+    }
+
+    fun pop(): Int {
+        peek()
+        return output.pop()
+    }
+
+    fun peek(): Int {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop())
+            }
+        }
+        return output.peek()
+    }
+
+    fun empty(): Boolean = input.isEmpty() && output.isEmpty()
+}
